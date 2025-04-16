63bd7713b4a13daaceac497b4fc950ec7cc667da
Marcos Paulo F Garcia <marcos.garcia@aluno.faculdadeimpacta.com.br>
2025-04-16 10:18:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ app.register_blueprint(professores_blueprint)
 app.register_blueprint(turmas_blueprint)
 
 if __name__ == '__main__':
-  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
+  app.run(host="0.0.0.0", port=5000)
