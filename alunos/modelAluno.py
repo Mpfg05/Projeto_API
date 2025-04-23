dc074693a2be6b9868e42fb431619b64ccf775fb
Marcos Paulo F Garcia <marcos.garcia@aluno.faculdadeimpacta.com.br>
2025-04-23 13:27:04 -0300

    Update modelAluno.py

--- alunos/modelAluno.py
+++ alunos/modelAluno.py
@@ -6,11 +6,11 @@ class Aluno(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
-    turma_id = db.Column(db.Integer, nullable=False)
+    turma_id = db.Column(db.Integer, db.ForeignKey('turma.id'), nullable=False)
     data_nascimento = db.Column(db.String(10), nullable=False)
     nota_primeiro_semestre = db.Column(db.Float, nullable=False)
     nota_segundo_semestre = db.Column(db.Float, nullable=False)
-    
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -20,7 +20,8 @@ class Aluno(db.Model):
             "data_nascimento": self.data_nascimento,
             "nota_primeiro_semestre": self.nota_primeiro_semestre,
             "nota_segundo_semestre": self.nota_segundo_semestre
-        }    
+        }
+
         
 def validar_nome(nome):
     return bool(re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ ]+$", nome))
